5a41f1db07a2331bbdadeb558c330a6d645c18c7
Lauren Lambert <lambertelauren@gmail.com>
2025-05-25 15:14:54 -0500

    deleted extra apollo client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,4 @@
 //import './index.css';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -19,38 +12,10 @@ document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundPosition = "center";
 document.body.style.backgroundAttachment = "fixed";
 
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-
-  // 🔍 Debugging output
-  if (token) {
-    console.log("✅ id_token found in localStorage:", token);
-  } else {
-    console.warn("⚠️ No id_token found in localStorage.");
-  }
-
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <Toaster position="top-center" reverseOrder={false} />
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 );
